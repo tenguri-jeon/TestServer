1293f2055a263f7008eb0d0edba9cf411f763c51
tenguri-jeon <110875363+tenguri-jeon@users.noreply.github.com>
2024-11-27 16:16:06 +0900

    server오류잡기

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // MySQL 연결
 const connection = mysql.createConnection({
-    host: 'localhost',  
+    host: '127.0.0.1' ,  
     port: 3306,       
     user: 'root',
     password: '12345',
